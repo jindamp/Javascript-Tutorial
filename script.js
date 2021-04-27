0aaa18aabb0dbfb9ff42244961debcd0c1adb71b
pavan <jindmpavan@gmail.com>
2021-04-27 09:19:37 +0530

    first 3 sessions

---
+++ script.js
@@ -0,0 +1,41 @@
+var x = 30;
+
+
+if(x<20){
+    console.log("x is less than 20");
+}else {
+    console.log("x is greater than 20");
+}
+
+
+console.log("============================");
+console.log("         For Loop         ");
+console.log("============================");
+
+var position = "top1";
+switch(position){
+	case "top":
+		console.log("Top");
+	    break;
+    default:
+        console.log("default")
+}
+
+
+for( var x=0; x < 10;  x++){
+	console.log( x );
+}
+
+
+console.log("============================");
+console.log("         While Loop         ");
+console.log("============================");
+
+var wx = 0;
+while( wx < 5)
+{
+	console.log(wx);
+	wx++;
+}
+
+
